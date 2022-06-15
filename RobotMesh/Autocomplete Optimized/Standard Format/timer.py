5bf8e3a7198b572a636e2ecf194b4b126cf91c80
Hunter D <71296258+HunterDorf@users.noreply.github.com>
2022-06-15 13:31:54 -0700

    Create timer.py

---
+++ RobotMesh/Autocomplete Optimized/Standard Format/timer.py
@@ -0,0 +1,39 @@
+class Timer:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def reset():
+        pass
+
+    @staticmethod
+    def start():
+        pass
+
+    @staticmethod
+    def stop():
+        pass
+
+    @staticmethod
+    def start_lap():
+        pass
+
+    @staticmethod
+    def is_running():
+        pass
+
+    @staticmethod
+    def elapsed_time(lap_index=-1):
+        pass
+
+    @staticmethod
+    def lap_count():
+        pass
+
+    @staticmethod
+    def total_time():
+        pass
+
+    @staticmethod
+    def average_time():
+        pass
