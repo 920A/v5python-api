ceb22c272c3636b620c3067476863e469197d755
Hunter D <71296258+HunterDorf@users.noreply.github.com>
2022-06-15 13:29:17 -0700

    Create timer.py
    https://www.robotmesh.com/studio/content/docs/vexv5-python/html/classtimer_1_1_timer.html

---
+++ RobotMesh/Complete Naming/timer.py
@@ -0,0 +1,30 @@
+class Timer:
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def start(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def start_lap(self):
+        pass
+
+    def is_running(self):
+        pass
+
+    def elapsed_time(self, lap_index=-1):
+        pass
+
+    def lap_count(self):
+        pass
+
+    def total_time(self):
+        pass
+
+    def average_time(self):
+        pass
